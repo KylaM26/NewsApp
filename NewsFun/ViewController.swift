c4d7e0e78b6899d316734205cb3265f1f4a52947
Kyla <kylawilson@Kylas-MBP.attlocal.net>
2019-06-04 17:37:43 -0500

    first commit

---
+++ NewsFun/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewsFun
+//
+//  Created by Kyla Wilson on 6/4/19.
+//  Copyright © 2019 Kyla Wilson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
